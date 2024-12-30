a3f528d106923f4a348c7e34e1eabff6c7adc11c
Pete Kazmier <pete@kazmier.com>
2024-12-30 08:57:51 -0500

    fix(matrix-hall): remove purple hint

--- matrix-hallway.glsl
+++ matrix-hallway.glsl
@@ -6,35 +6,19 @@
 //
 // https://www.shadertoy.com/view/ldjBW1
 //
-
-#define SPEED_MULTIPLIER 1.
 #define GREEN_ALPHA .33
+#define R fract(1e2*sin(p.x*8.+p.y))
+void mainImage(out vec4 o,vec2 u) {
+  vec3 v=vec3(u,1)/iResolution-.5,
+  s=.8/abs(v),
+  i=ceil(8e2*(s.z=min(s.y,s.x))*(s.y<s.x?v.xzz:v.zyz)),
+  j=fract(i*=.1),
+  p=vec3(9,int(iTime*(9.+8.*sin(i-=j).x)),0)+i;
+  o-=o,o.g=R/s.z;p*=j;o*=R>.5&&j.x<.6&&j.y<.8?GREEN_ALPHA:0.;
 
-#define BLACK_BLEND_THRESHOLD .4
-
-#define R fract(1e2 * sin(p.x * 8. + p.y))
-
-void mainImage(out vec4 fragColor, vec2 fragCoord) {
-    vec3 v = vec3(fragCoord, 1) / iResolution - .5;
-    // vec3 s = .5 / abs(v);
-    // scale?
-    vec3 s = .9 / abs(v);
-    s.z = min(s.y, s.x);
-    vec3 i = ceil( 8e2 * s.z * ( s.y < s.x ? v.xzz : v.zyz ) ) * .1;
-    vec3 j = fract(i);
-    i -= j;
-    vec3 p = vec3(9, int(iTime * SPEED_MULTIPLIER * (9. + 8. * sin(i).x)), 0) + i;
-    vec3 col = fragColor.rgb;
-    col.g = R / s.z;
-    p *= j;
-    col *= (R >.5 && j.x < .6 && j.y < .8) ? GREEN_ALPHA : 0.;
-
-  	// Sample the terminal screen texture including alpha channel
-    vec2 uv = fragCoord.xy / iResolution.xy;
-  	vec4 terminalColor = texture(iChannel0, uv);
-
-    float alpha = step(length(terminalColor.rgb), BLACK_BLEND_THRESHOLD);
-    vec3 blendedColor = mix(terminalColor.rgb * 1.2, col, alpha);
-
-    fragColor = vec4(blendedColor, terminalColor.a);
+  vec2 uv = u.xy / iResolution.xy;
+  vec4 terminalColor = texture(iChannel0, uv);
+  float mask = 1.2 - step(0.5, dot(terminalColor.rgb, vec3(1.0)));
+  vec3 blendedColor = mix(terminalColor.rgb * 1.2, o.rgb, mask);
+  o = vec4(blendedColor, terminalColor.a);
 }
